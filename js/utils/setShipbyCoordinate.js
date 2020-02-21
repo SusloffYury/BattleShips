ff393d6d5d86bc7d9db57d888dfc8db116e4e2bf
Suslov Yury <susloff.yury@gmail.com>
2020-02-21 12:21:16 +0300

    first commit

--- js/utils/setShipbyCoordinate.js
+++ js/utils/setShipbyCoordinate.js
@@ -4,10 +4,8 @@ export const setShipbyCoordinate =
  ({ coordinateX, coordinateY, directionX, directionY, decksShip } ) => {
    
     for (let i = coordinateX; i <= coordinateX + decksShip * directionX; i++) {
-                      console.log(`i = ${i}`)
         for (let j = coordinateY; j <= coordinateY + decksShip * directionY; j++) {
-          console.log(`i = ${j}`)
-            console.log(`i =${i} j =${j} `)
+           console.log(`i =${i} j =${j} `)
          document.querySelector(`#human [data--x = "${i}"][data--y = "${j}"]`)
          .dataset.ship = '1';
          
