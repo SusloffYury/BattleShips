a12bb052e2704d70fda8627f7f14286119cdc2f6
Suslov Yury <susloff.yury@gmail.com>
2020-02-25 15:30:32 +0300

    last_commit

---
+++ js/cleanRoot.js
@@ -0,0 +1,5 @@
+
+export const cleanRoot =()=>{
+ let root = document.querySelector('.form-signin');
+ root.innerHTML = '';
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-import{ startGame } from './startGame.js'
+import { startGame } from './startGame.js'
 
 import { createForm } from "./createForm.js";
 import { addFormSubmitEvent } from "./addFormEvents.js";
-
+import { cleanRoot } from './cleanRoot.js'
 
 if (!localStorage.getItem("isValidationRequired")) {
-    createForm();
-    addFormSubmitEvent();
-  } else {
-    console.log("СИСТЕМА ВАС ЗАПОМНИЛА, ВХОД УСПЕШЕН");
-    startGame();
-  }
+  createForm();
+  addFormSubmitEvent();
+} else {
+  console.log("СИСТЕМА ВАС ЗАПОМНИЛА, ВХОД УСПЕШЕН");
   
+  startGame();
+  cleanRoot();
+}
 
-  
+
